11c6a12d87c535352047a2dde62e49d2bc1e2738
melrah <edu.h.crss@gmail.com>
2019-11-08 17:19:45 +0100

    semi-update search

--- src/app/ricetta-lista/ricetta-lista.component.ts
+++ src/app/ricetta-lista/ricetta-lista.component.ts
@@ -11,12 +11,26 @@ export class RicettaListaComponent implements OnInit {
 
   ricette: Ricetta[];
   searchbar: boolean;
+  routeSub: any;
 
   constructor(private snackBar: MatSnackBar) {
     this.searchbar = false;
   }
 
   ngOnInit() {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+    this.db.searchRicetta(this.ricetta.tag).then(ricette => {
+      console.log(ricette);
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 
   }
